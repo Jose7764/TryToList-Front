b204376e4d6d5bd375ab026c39f61f5dba9ff0c6
GABRIEL EHRAT FAGUNDES <gabriel_e_fagundes@estudante.sesisenai.org.br>
2026-04-08 21:52:57 -0300

    chore: Adding registering and logging

--- api/users.js
+++ api/users.js
@@ -1,2 +1,31 @@
 const BASE_URL = 'http://localhost:8081/user';
 
+export async function saveUser(entries){
+    try{
+        const res = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(entries)
+        })
+
+        return await res.json();
+
+    }catch(e){
+        console.error("Error registering user: " + e.message);
+    }
+}
+
+export async function loginUser(entries){
+    try{
+        const res = await fetch(`${BASE_URL}/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(entries)
+        })
+
+        return await res.json();
+
+    }catch(e){
+        console.error("Error logging in user: " + e.message);
+    }
+}
--- front/auth.js
+++ front/auth.js
@@ -1,5 +1,39 @@
-const form = document.querySelector("#register");
+import { loginUser, saveUser } from "../api/users.js";
 
-form.addEventListener("submit", async (e) => {
-    
-});
+const formRegister = document.querySelector("#register");
+const formLogin = document.querySelector("#login");
+
+export function register(){  
+    formRegister.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(formRegister);
+        if(formData.get("password") !== formData.get("confirmPassword")) {
+            alert("Passwords do not match!");
+            return;
+        }
+
+        const entries = Object.fromEntries(formData.entries());
+        saveUser(entries).then(res => document.cookie=`current_user_id=${res.id}; expires=${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toUTCString()}; path=/`);
+
+        if(document.cookie.includes("current_user_id")){
+            document.location.href = "/index.html";
+        }
+    });
+}
+
+export function login(){
+    formLogin.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(formLogin);
+        const entries = Object.fromEntries(formData.entries());
+
+        // ADD A VERIFIER
+        loginUser(entries).then(res => document.cookie=`current_user_id=${res.id}; expires=${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toUTCString()}; path=/`);
+
+        if(document.cookie.includes("current_user_id")){
+            document.location.href = "./index.html";
+        }
+    });
+}
